d7998585cd7b4021ecbc6981537230a2e54d23c9
Murilovers27 <murilopiresandradecruz27@gmail.com>
2026-09-12 14:49:48 -0300

    Botão de play

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,17 @@ import Footer from "./components/Layout/Footer/Footer";
 import Header from "./components/Layout/Header/Header";
 import TeamCarousel from "./components/Home/TeamCarousel/TeamCarousel";
 import styles from "./page.module.css";
+import Hero from "./components/Home/Hero/Hero";
+import AboutStrategy from "./components/Home/AboutStrategy/AboutStrategy";
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <Header />
-      <main></main>
+      <main>
+        <Hero />
+        <AboutStrategy />
+      </main>
       <Footer />
     </div>
   );
